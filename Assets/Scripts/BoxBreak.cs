a692573f9f0a3ca8606290ffb5ebc702de8cbdd5
Nick <nicholasgin@gmail.com>
2024-05-05 20:56:02 -0400

    box break and feed fox menu

--- Assets/Scripts/BoxBreak.cs
+++ Assets/Scripts/BoxBreak.cs
@@ -2,11 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-
+using TMPro;
 public class BoxBreak : MonoBehaviour
 {
-    public List<GameObject> cornerSprites;
+		public List<GameObject> cornerSprites;
 		public GameObject babyFoxPayment;
+		public ItemCollector itemCollector;
+		public Button feedBtn;
+		public TMP_Text feedText;
+		private int pineapples;
+		
 
 		private void Start()
 		{
@@ -14,6 +19,8 @@ public class BoxBreak : MonoBehaviour
 				{
 						sprite.SetActive(false);
 				}
+
+				babyFoxPayment.gameObject.SetActive(false);
 		}
 
 		public void BreakBox()
@@ -22,6 +29,9 @@ public class BoxBreak : MonoBehaviour
 				{
 						sprite.SetActive(true);
 				}
+
+				StartCoroutine(DestroyBox());
+				
 		}
 
 		private void OnTriggerEnter2D(Collider2D collision)
@@ -32,10 +42,51 @@ public class BoxBreak : MonoBehaviour
 		private void OnTriggerExit2D(Collider2D collision)
 		{
 				//close menu
+				ClosePineapplesMenu();
 		}
 
 		public void OpenPineapplesMenu()
 		{
+				Color red = new Vector4(0.7849056f, 0.3463522f, 0.3317337f, 1f);
+				Color green = new Vector4(0.447f, 0.631f, 0.114f, 1f);
+
 				babyFoxPayment.SetActive(true);
+				pineapples = itemCollector.GetPineapples();
+
+				if (pineapples < 5)
+				{
+						// if <5, change text to say, not enough pinepplaes
+						// button shows Not enough pineapples, changes red = #C95955, green = #72a11d
+						feedBtn.image.color = red;
+						feedText.text = "Need More Food!";
+						feedText.fontSize = 28;
+
+				}
+				else if (pineapples >= 5)
+				{
+						// if >5, total -= 5, box disappears
+						feedBtn.image.color = green;
+						feedText.text = "Feed Fox";
+						feedText.fontSize = 29;
+				}
+
+
 		}
+
+		public void ClosePineapplesMenu()
+		{
+				babyFoxPayment.SetActive(false);
+		}
+
+		IEnumerator DestroyBox()
+		{
+				yield return new WaitForSeconds(1);
+				foreach (GameObject sprite in cornerSprites)
+				{
+						Destroy(sprite);
+				}
+
+				Destroy(this.gameObject);
+		}
+		
 }
--- Assets/Scripts/ItemCollector.cs
+++ Assets/Scripts/ItemCollector.cs
@@ -6,16 +6,18 @@ using UnityEngine;
 public class ItemCollector : MonoBehaviour
 {
     public int score = 0;
-    private int numPineapples;
+    public int numPineapples = 0;
     public TextMeshProUGUI ScoreText;
     [SerializeField] AudioSource collectSFX;
-    private void OnTriggerEnter2D(Collider2D collision)
+
+		private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Pineapple"))
         {
             collectSFX.Play();
             Destroy(collision.gameObject);
             numPineapples++;
+            Debug.Log(numPineapples + "pineapples");
             SetScore(25);
             PlayerPrefs.SetInt("score", score);
             ScoreText.text = "Points " + score;
@@ -33,6 +35,7 @@ public class ItemCollector : MonoBehaviour
 
     public int GetPineapples()
 		{
+        Debug.Log("Getpineapples");
         return numPineapples;
 		}
     public void SetScore(int val)
